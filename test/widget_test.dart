b606dc5346c4732899011f487f34d7951c5ca21e
weihuazheng6-dotcom <weihuazheng6@gmail.com>
2026-05-12 17:32:17 +0800

    Create widget_test.dart

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+// Placeholder test file to prevent flutter create from generating a broken one.
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('placeholder test', () {
+    expect(1 + 1, 2);
+  });
+}
